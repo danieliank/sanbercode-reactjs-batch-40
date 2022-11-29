9be0486733d36b16d2be8c26629438f8bc3c4a18
Daniel Ian <dikur1007@gmail.com>
2022-11-29 22:43:42 +0700

    Update Tugas 7

---
+++ tugas-reactjs/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Hi, Saya Daniel Ian Kurniawan</h1>
+        <hr></hr>
+        <p>Saya dari Batch 40 sudah berhasil menginstall React</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
